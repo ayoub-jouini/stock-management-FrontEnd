ecbd1efb1e3f1e1b0708e4f86291793300a3a3b7
ayoub-jouini <ayoubjouini135@gmail.com>
2024-04-29 16:45:55 +0100

    nav

---
+++ src/app/company/layout.tsx
@@ -0,0 +1,10 @@
+import Nav from "@/components/nav";
+
+export default function CompanyLayout() {
+  return (
+    <div>
+      <Nav />
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"></div>
+    </div>
+  );
+}
